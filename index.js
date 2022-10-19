1f283cb4418045bb2b554cc64b18b59aa406fef1
Yoppiiiiii <yoppi0513world@gmail.com>
2022-10-19 22:53:05 +0900

    test2

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function hasPenalty(records){
     // 関数を完成させてください
     for(let i = 1; i < records.length; i++){
-        if (records[i-1] > records[i]) {
-            return true;
+        if (records[i-1] < records[i]) {
+            return false;
         }
     }
-    return false;
+    return true;
 }
